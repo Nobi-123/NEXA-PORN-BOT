e8362f8690b0feed30b97a29af7c1d195b246991
Nobi-123 <nobita.infringement.report@gmail.com>
2025-11-23 16:30:33 +0530

    Update fsub.py

--- utils/fsub.py
+++ utils/fsub.py
@@ -1,1 +1,22 @@
+from pyrogram.errors import UserNotParticipant, BadRequest
+from config import FORCE_CHANNELS
 
+async def check_sub(client, user_id):
+    """
+    Checks if the user has joined all required channels
+    Returns True if joined, otherwise returns the channel they haven't joined
+    """
+    for ch in FORCE_CHANNELS:
+        if not ch:
+            continue
+        try:
+            mem = await client.get_chat_member(int(ch), user_id)
+            if mem.status in ("kicked",):
+                return ch
+        except UserNotParticipant:
+            return ch
+        except BadRequest:
+            return ch
+        except Exception:
+            return ch
+    return True
