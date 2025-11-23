886ee1e8b3e20b210733e5513417a60669280047
Nobi-123 <nobita.infringement.report@gmail.com>
2025-11-23 16:37:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+from pyrogram import Client
+from handlers import register_handlers
+from config import BOT_TOKEN, API_ID, API_HASH
 
+# ---------------- Create Pyrogram Client ----------------
+app = Client(
+    "anime_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+# ---------------- Register All Handlers ----------------
+register_handlers(app)
+
+# ---------------- Start Bot ----------------
+if __name__ == "__main__":
+    print("🤖 Lee Bsdk Telegram Bot is starting...")
+    app.run()
