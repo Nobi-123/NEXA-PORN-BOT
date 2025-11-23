fde77cb45024594c0528f2d025279d4708f85ebf
Nobi-123 <nobita.infringement.report@gmail.com>
2025-11-23 16:25:35 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,10 @@
+from .start import register_start
+from .video_handlers import register_video
+from .admin import register_admin
+from .scraper_handler import register_scraper
 
+def register_handlers(app):
+    register_start(app)
+    register_video(app)
+    register_admin(app)
+    register_scraper(app)
